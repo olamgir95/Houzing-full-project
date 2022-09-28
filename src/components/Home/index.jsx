2d9a1e66a2bcd2d5919925a8b3d17a2509d88541
Olamgir <ollomurodovolamgir233@gmail.com>
2022-09-28 21:42:47 +0900

    house card finished

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -2,12 +2,14 @@ import GenericCarousel from '../Carousel/';
 import React from 'react';
 
 import { Container } from './style';
+import HouseCard from '../HouseCard';
 
 
 export const Home = () => {
   return (
     <Container>
       <GenericCarousel/>
+      <HouseCard/>
     </Container>
   );
 };
